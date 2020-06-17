378a6c799df706343d838dc83f83ea3b61cab493
Hussam Eldin Mohamed <dr.hussam@live.com>
2020-06-17 17:37:31 -0400

    I added the person class

---
+++ COMP123-M2020-LAB4/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_LAB4
+{
+    class Person
+    {
+        //PRIVATE INSTANCE VARIABLE
+
+        //PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //CONSTRUCTOR
+        public Person(string name, int age)
+        {
+            //property initialization
+            Name = name;
+            Age = age;
+
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello");
+        }
+    }
+}
